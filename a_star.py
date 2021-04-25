ff8dfb30fd7d664985a2242c29175c28409ac7fd
Peter Wallace <pjwallace23@outlook.com>
2021-04-25 16:12:43 -0400

    a_star.py
    Modified the dijkstras.py file to run A*. Used the OSMnx Euclidian Distance function as the heuristic function to re-weigh edges.

---
+++ a_star.py
@@ -0,0 +1,69 @@
+import osmnx as ox
+import networkx as nx
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import heapq as heapq
+
+def a_star(G,source,target):
+    G_succ = G.succ
+    G_nodes = G.nodes
+
+    push = heapq.heappush
+    pop = heapq.heappop
+    heap = []
+    actual_distance = {source:0} #save actual distances of each node
+    marked = {}            #nodes that have been popped
+    #distance = {source:0} #current distance to each node
+    push(heap,(0,source))
+    x2 = G_nodes[target]['x']
+    y2 = G_nodes[target]['y']
+    while heap:
+        (h_d,v) = pop(heap)
+        d = actual_distance[v]
+        if v in marked:
+            continue        #v may have been popped bc skipping decrease key and just adding an extra
+        marked[v] = 1
+        if v == target:
+            break           #found the target node
+        for u, e in G_succ[v].items():  #u is next vertex number, e is information about the edge
+            cost = e[0]['length']
+            x1 = G_nodes[u]['x']
+            y1 = G_nodes[u]['y']
+            if(cost == None):
+                continue        #means data for the edge is missing
+            dist = d + cost
+            if u in marked:
+                continue
+            if u in actual_distance:
+                if dist < actual_distance[u]:
+                    actual_distance[u] = dist #save actual distance of u
+                    heur_d = ox.euclidian_dist_vector(y1,x1,y2,x2) #use euclidian distance from u to target for heap
+                    push(heap,(dist + heur_d,u)) #dont NEED to update distance
+            else:
+               actual_distance[u] = dist
+               push(heap,(dist,u))
+    return actual_distance
+
+
+#takes in the graph, source, target and distances and works backwards from the target following the
+#lowest weight path to the source
+def a_star_get_path(G,source,target,distances):
+    G_pred = G.pred
+    path = []
+    v = target
+
+    while v != source:
+        path.insert(0,v)
+        min = None
+        for u, e in G_pred[v].items():
+            if u in distances:
+                if min == None:
+                    min = distances.get(u)
+                    v = u
+                elif min > distances.get(u):
+                    min = distances.get(u)
+                    v = u
+    path.insert(0,v)
+    return path
