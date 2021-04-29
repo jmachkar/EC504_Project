9a0104a4957976b72332f81f6b675acf2a51c99a
Connor <60195626+connorgre@users.noreply.github.com>
2021-04-29 16:14:37 -0400

    Added loading from bigBoston.graphml
    This just makes it take about 3 minutes to load the graph from the file instead of about 6 minutes to download it

--- shortestPath.py
+++ shortestPath.py
@@ -9,12 +9,14 @@ import time
 from dijkstras import *
 from Astar import *
 
-start = (42.25,-71.05)
-end =   (42.40,-71.2)
+start = (42.0,-71.0)
+end =   (42.4,-71.4)
+
 #start = (42.3,-71.1)
 #end =   (42.35,-71.15)
 test = int(input("use default (1 or 0): "))
 if(test == 0):
+    print("If loading bigBoston.graphml as graph, 42.0 < latitude < 42.4; -71.4 < longitude < -71.0")
     s_lat = float(input("enter starting latitude: "))
     s_long = float(input("enter starting longitude: "))
 
@@ -32,16 +34,66 @@ north = max(start[0],end[0]) + .02
 south = min(start[0],end[0]) - .02
 east = max(start[1],end[1])  + .02
 west = min(start[1],end[1])  - .02
+load = int(input("load from bigBoston.graphml (1 or 0): "))
+if load == 0:
+    print("Generating graph:")
+    t0 = time.time()
+    G = ox.graph_from_bbox(north,south,east,west, network_type='drive')
 
-print("Generating graph:")
-t0 = time.time()
-G = ox.graph_from_bbox(north,south,east,west, network_type='drive')
+    Gp = ox.project_graph(G)
+    Gc = ox.consolidate_intersections(Gp, rebuild_graph=True, tolerance=20, dead_ends=False)
 
-Gp = ox.project_graph(G)
-Gc = ox.consolidate_intersections(Gp, rebuild_graph=True, tolerance=20, dead_ends=False)
+    t1 = time.time()
+    print("time to generate graph: ", t1-t0)
+elif load == 1:
+    print("loading graph:")
+    t0 = time.time()
+    #G = ox.io.load_graphml("bigBoston.graphml") cant use this bc it calls read_graphml wrong
+    # all this code is essentially just copying load_graphml and fixing what was wrong with load_graphml
+    default_node_dtypes = {
+        "elevation": float,
+        "elevation_res": float,
+        "lat": float,
+        "lon": float,
+        "osmid": int,
+        "street_count": int,
+        "x": float,
+        "y": float,
+    }
+    default_edge_dtypes = {
+        "bearing": float,
+        "grade": float,
+        "grade_abs": float,
+        "length": float,
+        "osmid": int,
+        "speed_kph": float,
+        "travel_time": float,
+    }
+    G = nx.read_graphml("bigBoston.graphml", node_type=default_node_dtypes["osmid"])
 
-t1 = time.time()
-print("time to generate graph: ", t1-t0)
+    G = ox.io._convert_node_attr_types(G, default_node_dtypes)
+    G = ox.io._convert_edge_attr_types(G, default_edge_dtypes)
+
+    # remove node_default and edge_default metadata keys if they exist
+    if "node_default" in G.graph:
+        del G.graph["node_default"]
+    if "edge_default" in G.graph:
+        del G.graph["edge_default"]
+
+    Gp = ox.project_graph(G)
+    Gc = ox.consolidate_intersections(Gp, rebuild_graph=True, tolerance=20, dead_ends=False)
+    t1 = time.time()
+    print("time to load graph: ",t1-t0)
+else:
+    print("invalid input")
+    exit(-1)
+
+
+#print("starting to save graph...")
+#t0 = time.time()
+#ox.io.save_graphml(G,filepath="bigBoston.graphml")
+#t1 = time.time()
+#print("time to save graph: ", t1-t0)
 
 nodes, edges = ox.graph_to_gdfs(Gc, nodes=True, edges=True)
 
